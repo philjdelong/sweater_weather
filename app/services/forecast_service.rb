cb0b0b0992847e158d0166227ecb5959b814935c
Phil DeLong <philjdelong@gmail.com>
2020-03-01 18:22:52 -0700

    Create forecast service

---
+++ app/services/forecast_service.rb
@@ -0,0 +1,47 @@
+class ForecastService
+
+    def initialize(location)
+        coordinates ||= GeoLocationService.new(location)
+        @lat = coordinates.latitude
+        @lng = coordinates.longitude
+    end
+
+    def data
+        get_json
+    end
+
+    def currently
+        data["currently"]
+    end
+
+    def minutely
+        data["minutely"]
+    end
+
+    def hourly
+        data["hourly"]
+    end
+
+    def daily
+        data["daily"]
+    end
+
+    def alerts
+        data["alerts"]
+    end
+
+    private
+
+        def get_json
+            JSON.parse(connection.get.env['response_body'])
+        end
+
+        def connection
+            key = ENV['DARK_SKY_KEY']
+
+            return @conn if @conn
+            @conn = Faraday.new(url: "https://api.darksky.net/forecast/#{key}/#{@lat},#{@lng}") do |f|
+                f.adapter Faraday.default_adapter
+            end
+        end
+end
