2e5225c514b329c163ab6914406aabaabd3bd4f2
Phil DeLong <philjdelong@gmail.com>
2020-03-03 14:27:00 -0700

    Update spec for antipode

--- spec/requests/api/v1/antipode_spec.rb
+++ spec/requests/api/v1/antipode_spec.rb
@@ -1,21 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Antipode endpoint" do
-    it "sends id" do
+    it "sends name and forecast" do
         city = 'Hong Kong'
 
         get "/api/v1/antipode?location=#{city}"
         expect(response).to be_successful
 
-        response = JSON.parse(response.body)
+        antipode = JSON.parse(response.body)
 
-        expect(response['data']['attributes'].keys).to eq(['location_name', 'forecast'])
-        expect(response['data']['attributes']['forecast'].keys).to eq(['summary', 'current_tempurature'])
+        expect(antipode['data']['attributes'].keys).to eq(['location_name', 'forecast'])
+        expect(antipode['data']['attributes']['location_name']).to eq("RP69, Jujuy, Argentina")
+        expect(antipode['data']['attributes']['forecast'].keys).to eq(['summary', 'current_tempurature'])
+
+        expect(antipode['data']['attributes']['forecast']['summary'].class).to eq(String)
+        expect(antipode['data']['attributes']['forecast']['current_tempurature'].class).to eq(Float)
     end
 end
-
-
-# http://amypode.herokuapp.com/api/v1/antipodes?lat=27&long=-82
 
 # {
 #     "data": [
