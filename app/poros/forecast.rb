6bc36668834fd1274cc5c5ff2dd52ee3122b9135
Phil DeLong <philjdelong@gmail.com>
2020-03-02 20:56:10 -0700

    Remove unused methods

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -10,19 +10,11 @@ class Forecast
         ForecastService.new(@location).currently
     end
 
-    def minutely
-        ForecastService.new(@location).minutely
-    end
-
     def hourly
         ForecastService.new(@location).hourly
     end
 
     def daily
         ForecastService.new(@location).daily
-    end
-
-    def alerts
-        ForecastService.new(@location).alerts
     end
 end
