bf793ebad899f4d9d493e2dc1c2f099f5b2b8fe5
Phil DeLong <philjdelong@gmail.com>
2020-03-03 00:13:59 -0700

    Create backgrounds controller

---
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::BackgroundsController < ApplicationController
+
+    def index
+        background = Background.new(params['location'])
+        render json: BackgroundSerializer.new(background)
+    end
+end
