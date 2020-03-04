71bb13b9d16428b5da912756f0afab4f275d7cf1
Phil DeLong <philjdelong@gmail.com>
2020-03-03 17:58:22 -0700

    Create user model

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+    validates :email, uniqueness: true, presence: true
+
+    validates_confirmation_of :password
+    validates_presence_of :password_digest, require: true
+    has_secure_password
+
+    validates :api_key, uniqueness: true
+    has_secure_token :api_key
+  end
