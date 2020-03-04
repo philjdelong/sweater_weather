2b3c8614538c29af681a18a079dda9d792a6b5c3
Phil DeLong <philjdelong@gmail.com>
2020-03-04 08:13:14 -0700

    Update user for test

--- spec/requests/api/v1/user_login_spec.rb
+++ spec/requests/api/v1/user_login_spec.rb
@@ -4,18 +4,18 @@ RSpec.describe "User Login" do
     describe "Endpoint" do
         it "shows 200 response and api_key with successful login" do
             post "/api/v1/users", params:{
-            email: 'phil@example.com',
-            password: 'password',
-            password_confirmation: 'password'
-        }
+                email: 'phil@example.com',
+                password: 'password',
+                password_confirmation: 'password'
+            }
 
-            phil = User.create(id: 1, email: 'phil@example.com', password_digest: 'abcdefg')
+            phil = User.last
 
             post "/api/v1/sessions", params:{
                 email: "#{phil.email}",
                 password: "#{phil.password_digest}"
             }
-            
+
             expect(response).to be_successful
             expect(response.status).to eq(200)
 
@@ -25,13 +25,11 @@ RSpec.describe "User Login" do
 
         it "gets 400 error if credentials are incorrect" do
             
-            phil = User.create(id: 1, email: 'phil@example.com', password_digest: 'abcdefg')
-
             post "/api/v1/users", params:{
                 email: 'phil@example.com',
                 password: 'wrong',
             }
-    
+            
             expect(response.status).to eq(400)
         end
     end
