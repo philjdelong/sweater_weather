af2cd3c2db71da3e9fd9822fdd248c934eaf5d8d
Phil DeLong <philjdelong@gmail.com>
2020-03-04 09:53:30 -0700

    Add road_trip route

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
          resources :users, only: [:create]
 
          resources :sessions, only: [:create]
+
+         resources :road_trip, only: [:create]
       end
    end
 end
