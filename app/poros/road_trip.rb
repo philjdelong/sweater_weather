70f3b1388bf26631aa24861fd656645dccac24df
Phil DeLong <philjdelong@gmail.com>
2020-03-04 10:55:40 -0700

    Create roadtrip poro

---
+++ app/poros/road_trip.rb
@@ -0,0 +1,30 @@
+class RoadTrip
+    attr_reader :id
+
+    def initialize(trip_info)
+        @id = nil
+        @end_location = trip_info[:end]
+        @start_location = trip_info[:start]
+    end
+
+    def start_location
+        @start_location
+    end
+
+    def end_location
+        @end_location
+    end
+
+    def travel_time
+        GoogleDirectionsService.new(@start_location, @end_location).travel_time
+    end
+
+    def forecast
+        current = Time.now.to_i
+        travel = travel_time['value']
+        arrival = (current + travel)
+        
+        x = ForecastService.new(@end_location, arrival)
+        JSON.parse(x.future_forecast.body)['currently']['summary']
+    end
+end
